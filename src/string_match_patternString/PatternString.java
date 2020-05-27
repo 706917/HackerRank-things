17ed4af1245c592727a6ebfa4a7d4045b6716bdb
Alex <githubalexrom@gmail.com>
2020-05-27 16:31:03 -0400

    Create PatternString.java

---
+++ src/string_match_patternString/PatternString.java
@@ -0,0 +1,44 @@
+/*
+ * You are given two strings pattern and s. The first string pattern contains only the symbols 0 and 1, and the second string s contains only lowercase English letters.
+
+Let's say that pattern matches a substring s[l..r] of s if the following 3 conditions are met:
+
+they have equal length;
+for each 0 in pattern the corresponding letter in the substring is a vowel;
+for each 1 in pattern the corresponding letter is a consonant.
+Your task is to calculate the number of substrings of s that match pattern.
+
+Note: In this task we define the vowels as 'a', 'e', 'i', 'o', 'u', and 'y'. All other letters are consonants.
+ */
+
+
+package string_match_patternString;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class PatternString {
+	
+	public static void main(String[] args) {
+	
+	String s = "abba";
+	
+	String t = "bsbss";
+//	System.out.print(s.contains("" + 'a'));
+	int a = '-';
+	System.out.println(a);
+	
+	
+	List <String> list = new ArrayList<>();
+    list.add(s);
+    list.add(t);
+    Collections.sort(list);
+    if(list.get(0).equals(t)) 
+    	Arrays.sort(arr);
+    
+	}
+	
+	
+}
