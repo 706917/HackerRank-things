345f58ba0368f9fb1b386881ccec6fde5852cb67
Alex <githubalexrom@gmail.com>
2020-06-04 15:07:09 -0400

    Create Solution.java

---
+++ src/bitwaiseAND/Solution.java
@@ -0,0 +1,52 @@
+/*
+ * Task
+Given set . Find two integers,  and  (where ), from set  such that the value of  is the maximum possible and also less than a given integer, . In this case,  represents the bitwise AND operator.
+
+Input Format
+
+The first line contains an integer, , the number of test cases.
+Each of the  subsequent lines defines a test case as  space-separated integers,  and , respectively.
+
+Constraints
+
+Output Format
+
+For each test case, print the maximum possible value of  on a new line.
+ */
+
+package bitwaiseAND;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nk = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nk[0]);
+
+            int k = Integer.parseInt(nk[1]);
+            
+            int max = 0;
+            int x = 0;
+            for (int i = 1; i<k; i++){
+            	
+                if(Byte.) {
+                }             
+            }
+            System.out.print(x&(x+1));
+            
+        }
+
+        scanner.close();
+
+        
+    }
+
+}
